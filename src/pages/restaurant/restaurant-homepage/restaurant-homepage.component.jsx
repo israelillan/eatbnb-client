74f7fb47f622d016d46c796bb4c753529f70b11e
israelillan <isra@arklay.es>
2021-07-19 09:19:15 +0200

    Beautified reservations report

--- src/pages/restaurant/restaurant-homepage/restaurant-homepage.component.jsx
+++ src/pages/restaurant/restaurant-homepage/restaurant-homepage.component.jsx
@@ -1,60 +1,72 @@
 import React, {useEffect, useState} from 'react';
-import {RestaurantPageContainer} from "./restaurant-homepage.styles";
 import {connect} from "react-redux";
 import {createStructuredSelector} from "reselect";
+import {Col, Row} from "react-bootstrap";
 import {DatePicker} from "@material-ui/lab";
-import {Divider, List, ListItemText, TextField} from "@material-ui/core";
+import {Container, Divider, Grid, Paper, Stack, TextField} from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
+import {format} from "date-fns";
 
 import {selectReservationsReport} from "../../../redux/reservation/reservation.selectors";
 import {getReservationsReportStart} from "../../../redux/reservation/reservation.actions";
-import {format} from "date-fns";
+import {BigCenteredContainer} from "../../../components/common-styles/common.styles";
 
 const RestaurantHomePage = ({reservationsReport, getReservationsReport}) => {
     const [selectedDate, setSelectedDate] = useState(new Date());
 
-    useEffect(() => getReservationsReport(selectedDate), [setSelectedDate]);
+    useEffect(() => getReservationsReport(selectedDate), [selectedDate, getReservationsReport]);
 
     const reservationReportListItems = [];
     for (let tableReference in reservationsReport) {
-        reservationReportListItems.push(<ListItemText key={tableReference} primary={`Table #${tableReference}`}/>);
-        reservationReportListItems.push(<Divider/>);
-
-        let reservationIndex = 0;
+        let tableReservations = []
         for (let reservation of reservationsReport[tableReference].reservations) {
-            const reservationListItem = <List>
-                <ListItemText key={reservationIndex}
-                              primary={`${format(reservation.dateAndTime, 'kk')}:00: ${reservation.customerName} - ${reservation.customerPhone}`}/>
-            </List>;
-            reservationReportListItems.push(reservationListItem);
-            reservationIndex++;
+            const reservationListItem =
+                <Stack key={tableReservations.length}>
+                    <Typography variant='body1'>
+                        {`${format(reservation.dateAndTime, 'kk')}:00: ${reservation.customerName} - ${reservation.customerPhone}`}
+                    </Typography>
+                </Stack>;
+            tableReservations.push(reservationListItem);
         }
+        reservationReportListItems.push(
+            <Grid key={tableReference} item xs>
+                <Paper>
+                    <Typography variant='h5'>{`Table #${tableReference}`}</Typography>
+                    <Divider />
+                    {tableReservations}
+                </Paper>
+            </Grid>);
     }
 
-    return <RestaurantPageContainer>
-        <DatePicker
-            label=""
-            value={selectedDate}
-            onChange={(newValue) => {
-                getReservationsReport(newValue);
-                setSelectedDate(newValue);
-            }}
-            renderInput={(params) => <TextField {...params} />}
-        />
-        <br/>
-        <List
-            sx={{width: '100%'}}
-            component="nav"
-        >
-            {reservationReportListItems}
-        </List>
-    </RestaurantPageContainer>;
+    return <BigCenteredContainer>
+        <Row>
+            <Col>
+                <Container maxWidth='sm'>
+                    <DatePicker
+                        label=""
+                        value={selectedDate}
+                        onChange={(newValue) => {
+                            getReservationsReport(newValue);
+                            setSelectedDate(newValue);
+                        }}
+                        renderInput={(params) => <TextField {...params} />}
+                    />
+                </Container>
+            </Col>
+        </Row>
+        <Row>
+            <Col>
+                <Grid container spacing={3} alignItems='flex-start' justifyContent='center'>
+                    {reservationReportListItems}
+                </Grid>
+            </Col>
+        </Row>
+    </BigCenteredContainer>;
 };
 
-export default connect(
-    createStructuredSelector({
+export default connect(createStructuredSelector({
         reservationsReport: selectReservationsReport
     }),
     dispatch => ({
         getReservationsReport: (date) => dispatch(getReservationsReportStart(date)),
-    })
-)(RestaurantHomePage);
+    }))(RestaurantHomePage);
